cc9c29fab3929541a42bfd08d7d04b263f39d544
Pythonic-Rainbow <wongchingho0@gmail.com>
2025-06-21 01:00:58 +0800

    server: Added Player script

---
+++ src/client/init.client.lua
@@ -0,0 +1,15 @@
+print("Hello world, from client!")
+
+--[[ local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+print("Local player is: " .. player.Name)
+
+
+print("Running client script")
+print(player:GetAttribute("IsStoreOpened"))
+print("Trying to set")
+player:SetAttribute("IsStoreOpened", true)
+print("Trying to get again")
+print(player:GetAttribute("IsStoreOpened"))
+print("Done") ]]
--- src/server/Player.server.lua
+++ src/server/Player.server.lua
@@ -1,8 +1,7 @@
 local Players = game:GetService("Players")
 
+-- Player attributes defined in server is replicated to all clients
+-- Each client can modify their own attributes but these changes will NOT be reflected on the server and all other clients
 Players.PlayerAdded:Connect(function(player)
-    local isStoreOpened = Instance.new("BoolValue")
-	isStoreOpened.Name = "IsStoreOpened"
-	isStoreOpened.Value = false
-	isStoreOpened.Parent = player
+	player:SetAttribute("IsStoreOpened", false)
 end)
